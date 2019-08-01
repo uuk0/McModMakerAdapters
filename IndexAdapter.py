c0d41fefce73fa2036b4a4fc1f25878b5cbbd98d
uuk0 <baulukas1301@googlemail.com>
2019-08-01 16:23:57 +0200

    filled some methods with code

--- IndexAdapter.py
+++ IndexAdapter.py
@@ -2,16 +2,19 @@
 Main interface for the index.json file. Provides function to read informations out of it
 """
 
+import json
+
 
 class IndexAdapter:
     def __init__(self, file):
-        pass
+        with open(file) as f:
+            self.data = json.load(f)
 
     def get_mcmodmaker_build_ids(self) -> list:
-        pass
+        return self.data["forge_versions"].keys()
 
     def get_supported_forge_build_ids(self) -> list:
-        pass
+        return self.data["mcmodmaker_versions"].keys()
 
     def get_adapters_for_forge_build_and_mcmodmaker_build(self, forge, mcmodmaker) -> list:
         pass
